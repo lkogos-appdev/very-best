c67b8c007639861908541ffce1d07c0a90df17c8
Larry Kogos <lkogos@chicagobooth.edu>
2018-05-28 19:00:27 +0000

    changed home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root :to => "venues#index"
 
+
+
+ root "dishes#index"
+ 
   # Routes for the Bookmark resource:
   # CREATE
   get "/bookmarks/new", :controller => "bookmarks", :action => "new"
